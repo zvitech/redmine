704cea7c7e9f5d06ab2eeea67a585a14c5cc4909
Alberto Mijares <granalberto@users.noreply.github.com>
2024-11-27 21:06:38 -0400

    Update redmine_ticket_creator.pl
    fixing constructor

--- redmine_ticket_creator.pl
+++ redmine_ticket_creator.pl
@@ -36,7 +36,7 @@ if (!defined $api_key || !defined $tracker_id || !defined $url) {
 # Set up Redmine client
 $logger->info("Setting up Redmine client with URL: $url and API Key: $api_key");
 my $redmine = Redmine::API->new(
-    url     => $url,
+    base_url     => $url,
     api_key => $api_key
 );
 
